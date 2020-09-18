05b2c20f1a46d29586e9445b7c50b8217214d220
heidiali <heidi.alikippari@gmail.com>
2020-09-18 17:49:49 +0300

    Change intro text and made picture responsive

--- src/contents/About.js
+++ src/contents/About.js
@@ -5,22 +5,19 @@ class About extends Component {
     render() {
         return (
             <div className="condiv">
-            <h1 className="subtopic"> <i>About Me </i></h1>
-            <h4>Hey stranger! </h4>
-            <h1>I'm Heidi</h1>
-            <h3>Full Stack Web Developer | Bioscientist </h3>
-            <br></br>
-            <p>I started my career as a bioscientist, but doing image and data analysis
-                 quickly came to appreciate just how much software and coding skills can aid us. 
-                So started my journey into the world of computers. I'm now working as a full stack developer.
-                <br/>
-                Web development is my center of interest, I like the idea of cross-platform development, 
-                which web technology like Javascript and .NET Core enables me to do. I also like making solutions that make everyday life easier.
-            </p>
-            
+                <h1 className="subtopic"> <i>About Me </i></h1>
+                <h2>I'm Heidi</h2>
+                <h3>Full Stack Web Developer | Bioscientist </h3>
+                <br></br>
+                <p> Experience from biosciences among research, customer service and project management in multicultural work environments. Programming experience as junior full stack web developer. </p>
+                <br></br>
+                <p> I started my career as a bioscientist, but doing image and data analysis I quickly came to appreciate just how much software and coding skills can aid us. So thus started my journey into the world of computers. Web development is my center of interest, I like the idea of cross-platform development,
+                    which web technology like Javascript and .NET Core enables me to do. I also like making solutions that make the everyday life easier. </p>
+                <br></br>
+                <p> I enjoy new challenges and continuous learning. My core strengths are great organizational and communication skills as well as ability to learn new things fast. </p>
             </div>
-            )
-        }
+        )
     }
-    
+}
+
 export default About
--- src/contents/Home.js
+++ src/contents/Home.js
@@ -3,16 +3,13 @@ import ReactTypingEffect from 'react-typing-effect';
 import profilepic from '../img/heidi.jpg';
 import Social from '../components/Social'
 
-
 class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-            <img src={profilepic} className="profilepic" 
-            style={{height:200, width:300}} >
-             </img>
+            <img src={profilepic} className="profilepic"></img>
             <br/>
-            <ReactTypingEffect className="typingeffect" text={['Hi there! I am Heidi',' I am a web developer']} speed={100} eraseDelay={700}/>
+            <ReactTypingEffect className="typingeffect" text={['Hello and welcome! My name is Heidi.']} speed={100} eraseDelay={700}/>
             
             <Social />
             </div>
